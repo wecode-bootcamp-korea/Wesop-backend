34bf36984c5596533ecbc593e3e7dbf3c4c2f671
Sam <klioop2@gmail.com>
2021-01-15 14:43:50 +0900

    ADD: ProductRetrieveView 생성

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views      import ProductListView, ProductRetrieveView
 
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('', ProductListView.as_view()),
+    path('/<int:id>', ProductRetrieveView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -48,6 +48,59 @@ class ProductListView(View):
             return JsonResponse({
                 "products": my_product
             }, status=200)
+
         except:
-            return HttpResponse("BAD_REQUEST: 404")
+            return HttpResponse("BAD_REQUEST", status=404)
+
+class ProductRetrieveView(View):
+    def get_object(self, id):
+        try:
+            return Product.objects.get(pk=id)
+        except:
+            return HttpResponse("NOT_FOUND", stauts=404)
+
+    def get(self, request, id):
+        product = self.get_object(id)
+
+        subcategories    = product.subcategories.all()
+        subcategory_info = {f"subcategory_{i}": subcat.name for i,subcat in enumerate(subcategories)}
+        
+        skin_types = product.skin_types.all()
+        skin_info  = {f"type_{i}": skin.name for i, skin in enumerate(skin_types)}
+
+        feels     = product.feels.all()
+        feel_info = {f"feel_{i}": feel.name for i, feel in enumerate(feels)}
+        
+        ingredients     = product.ingredients.all()
+        ingredient_info = {f"ingredient_{i}": ingredient.name for i, ingredient in enumerate(ingredients)}
+        
+        textures     = product.textures.all()
+        texture_info = {f"texture_{i}": texture.name for i, texture in enumerate(textures)}
+        
+        aromas     = product.aromas.all()
+        aroma_info = {f"aroma_{i}": aroma.name for i, aroma in enumerate(aromas)}
+
+        product_info = {
+            "id"           : product.pk,
+            "name"         : product.name,
+            "size"         : product.size,
+            "dosage"       : product.dosage,
+            "description"  : product.description,
+            "manual"       : product.manual,
+            "price"        : product.price,
+            "subcategories": subcategory_info,
+            "skin_types"   : skin_info,
+            "feels"        : feel_info,
+            "ingredients"  : ingredient_info,
+            "textures"     : texture_info,
+            "aromas"       : aroma_info,
+        }
+
+        return JsonResponse({
+            "product":product_info
+        }, status=200)
+            
+    
+    
+        
         
