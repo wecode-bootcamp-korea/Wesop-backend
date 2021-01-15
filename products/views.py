f400c17c73036d6240f6d287a48c653cc368ab4f
Sam <klioop2@gmail.com>
2021-01-15 13:53:22 +0900

    MODIFY: ProductListView 컨벤션 반영 및 오타수정

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views import View
-from django.http import JsonResponse, HttpResponse
+from django.views   import View
+from django.http    import JsonResponse, HttpResponse
 
 from products.models import Product
 
@@ -10,38 +10,38 @@ class ProductListView(View):
 
             my_product = []
             for product in products:
-                subcategories = product.subcategories.all()
+                subcategories    = product.subcategories.all()
                 subcategory_info = {f"subcategory_{i}": subcat.name for i,subcat in enumerate(subcategories)}
                 
                 skin_types = product.skin_types.all()
-                skin_info = {f"type_{i}": skin.name for i, skin in enumerate(skin_types)}
+                skin_info  = {f"type_{i}": skin.name for i, skin in enumerate(skin_types)}
 
-                feels = product.feels.all()
+                feels     = product.feels.all()
                 feel_info = {f"feel_{i}": feel.name for i, feel in enumerate(feels)}
                 
-                ingredients = product.ingredients.all()
+                ingredients     = product.ingredients.all()
                 ingredient_info = {f"ingredient_{i}": ingredient.name for i, ingredient in enumerate(ingredients)}
                 
-                textures = product.textures.all()
+                textures     = product.textures.all()
                 texture_info = {f"texture_{i}": texture.name for i, texture in enumerate(textures)}
                 
-                aromas = product.aromas.all()
+                aromas     = product.aromas.all()
                 aroma_info = {f"aroma_{i}": aroma.name for i, aroma in enumerate(aromas)}
 
                 product_info = {
-                    "id": product.pk,
-                    "name": product.name,
-                    "size": product.size,
-                    "dosage": product.dosage,
-                    "description": product.description,
-                    "manual": product.manual,
-                    "price": product.price,
+                    "id"           : product.pk,
+                    "name"         : product.name,
+                    "size"         : product.size,
+                    "dosage"       : product.dosage,
+                    "description"  : product.description,
+                    "manual"       : product.manual,
+                    "price"        : product.price,
                     "subcategories": subcategory_info,
-                    "skin_types": skin_info,
-                    "feels": feel_info,
-                    "ingredients": ingredient_info,
-                    "textures": texture_info,
-                    "aromas": aroma_info,
+                    "skin_types"   : skin_info,
+                    "feels"        : feel_info,
+                    "ingredients"  : ingredient_info,
+                    "textures"     : texture_info,
+                    "aromas"       : aroma_info,
                 }
                 my_product.append(product_info)
 
@@ -49,5 +49,5 @@ class ProductListView(View):
                 "products": my_product
             }, status=200)
         except:
-            return HttpResponse("BADREQUET: 404")
+            return HttpResponse("BAD_REQUEST: 404")
         
